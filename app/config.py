3a3b24e17fb8249b30a33515bc70a24c52de8bfc
Alexey <ae.polyakov@mail.ru>
2026-02-10 18:33:33 +0400

    init

---
+++ app/config.py
@@ -0,0 +1,21 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: str
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+
+    SECRET_KEY: str
+    DEBUG: bool
+
+    model_config = SettingsConfigDict(extra="ignore", env_file=".env", case_sensitive=True)
+
+
+settings = Settings()
