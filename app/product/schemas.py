9befda841fe03da764b8e0d03fd0381780741d54
Alexey <ae.polyakov@mail.ru>
2026-02-18 21:39:31 +0400

    category in product

--- app/product/schemas.py
+++ app/product/schemas.py
@@ -1,12 +1,14 @@
 from pydantic import BaseModel
 
+from app.category.schemas import CategoryCreateSchema
 from app.core.schemas import IdMixin, CreatedUpdatedMixin
 
 
 class ProductCreateSchema(BaseModel):
     name: str
     price: float
+    category: CategoryCreateSchema | None = None
 
 
 class ProductUpdateSchema(ProductCreateSchema, IdMixin, CreatedUpdatedMixin):
-    pass
+    category_id: int | None = None
--- app/product/service.py
+++ app/product/service.py
@@ -1,10 +1,16 @@
-from app.product.models import Product
+from sqlalchemy import Select, select
+from sqlalchemy.orm import joinedload
+
 from app.core.crud import CRUDBase
+from app.product.models import Product
 from app.product.schemas import ProductCreateSchema, ProductUpdateSchema
 
 
 class ProductService(CRUDBase[Product, ProductCreateSchema, ProductUpdateSchema]):
-    pass
+
+    @property
+    def base_select(self) -> Select:
+        return select(Product).options(joinedload(Product.category))
 
 
 product_service = ProductService(Product)
