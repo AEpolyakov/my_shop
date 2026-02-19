d3deb95414ead2e4457298072342768ddb9ca208
Alexey <ae.polyakov@mail.ru>
2026-02-19 19:13:48 +0400

    users table

--- app/models.py
+++ app/models.py
@@ -1,2 +1,3 @@
 from .product.models import Product
 from .category.models import Category
+from .user.models import User
---
+++ app/user/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, String
+
+from app.core.models import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    email = Column(String(255), unique=True, nullable=False, index=True)
+    password_hash = Column(String(255), nullable=False)
+    name = Column(String(255), nullable=True)
