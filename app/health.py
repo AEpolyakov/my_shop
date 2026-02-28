2e20e5a7f4c4ed6f03be8d3a9b0011c4be53aab8
alexey <ae.polyakov@mail.ru>
2026-02-28 12:09:33 +0400

    health

--- app/health.py
+++ app/health.py
@@ -3,14 +3,14 @@ from sqlalchemy.orm import Session
 
 from app.core.session import get_db
 
-health_router = APIRouter()
+health_router = APIRouter(prefix="/health")
 
 
-@health_router.get("/health_db")
-async def test_endpoint(db: Session = Depends(get_db)):
+@health_router.get("db")
+async def db_health(db: Session = Depends(get_db)):
     return {"message": "API is working"}
 
 
-@health_router.get("/health")
-async def health():
+@health_router.get("app")
+async def app_health():
     return {"status": "ok"}
